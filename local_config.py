0e94821a1d9f422678a90c7a2604ada998392a2d
bricenf <bricen2002@gmail.com>
2024-10-09 09:27:52 +0200

    API addition

---
+++ local_config.py
@@ -0,0 +1,7 @@
+am_i_local = True
+https = "insertYourProxyHere"
+http = "insertYourProxyHere"
+apikey_id = "dn2nj9wj43g3tqjfqm1usaww8	"
+apikey_secret = "4xk3pcu9zvbg0sbqt3m9h2h8ul9pwno3ail41fx32sribsc7ht"
+apptoken = "E3xdgxFdEuwC8EYogY50QwVas"
+apptoken_secret = "h1jfquqt9VVHrRB0XckAJsp1SvGkNhsd9SOy"
